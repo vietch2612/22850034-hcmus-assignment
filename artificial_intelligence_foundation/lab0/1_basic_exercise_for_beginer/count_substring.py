8affcb30bff1ce5df00c7da302d60e466b520bee
Viet Cao <viet.ch2612@gmail.com>
2022-12-29 14:01:41 +0700

    Basic exercies

---
+++ artificial_intelligence_foundation/lab0/1_basic_exercise_for_beginer/count_substring.py
@@ -0,0 +1,6 @@
+# Return the count of a given substring from a string
+# Given str_x = "Emma is good developer. Emma is a writer"
+# Return Emma appeared 2 times
+
+string_x = "Emma is good developer. Emma is a writer"
+print('Emma appeared ' + str(string_x.count('Emma')) + ' time(s).')
