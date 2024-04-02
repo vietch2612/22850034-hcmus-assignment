b720b3b6f8416542461f365e596d3b5717e141b8
Viet Cao <viet.ch2612@gmail.com>
2024-04-03 00:50:41 +0700

    Added more comments and refactor to improve performance

--- data_organization/ex4/VIQR_UTF8.cpp
+++ data_organization/ex4/VIQR_UTF8.cpp
@@ -6,65 +6,57 @@
 #include <unordered_map>
 #include <vector>
 
+// These mappings should be in the same length and order
+// And should be sorted by the length of the first element in descending order
+// Example: "e^`" should be before "e^"
+// I did not sort it on runtime to increase performance
 std::vector<std::string> viqrChars = {
-    "A`",  "A'",  "A^",  "A~",  "E`",  "E'",  "E^",  "I`",  "I'",  "O`",  "O'",
-    "O^",  "O~",  "U`",  "U'",  "Y'",  "a`",  "a'",  "a^",  "a~",  "e`",  "e'",
-    "e^",  "i`",  "i'",  "o`",  "o'",  "o^",  "o~",  "u`",  "u'",  "y'",  "A(",
-    "a(",  "DD",  "dd",  "I~",  "i~",  "U~",  "u~",  "O*",  "o*",  "U*",  "u*",
-    "A.",  "a.",  "A?",  "a?",  "A^'", "a^'", "A^`", "a^`", "A^?", "a^?", "A^~",
-    "a^~", "A^.", "a^.", "A('", "a('", "A(`", "a(`", "A(?", "a(?", "A(~", "a(~",
-    "A(.", "a(.", "E.",  "e.",  "E?",  "e?",  "E~",  "e~",  "E^'", "e^'", "E^`",
-    "e^`", "E^?", "e^?", "E^~", "e^~", "E^.", "e^.", "I?",  "i?",  "I.",  "i.",
-    "O.",  "o.",  "O?",  "o?",  "O^'", "o^'", "O^`", "o^`", "O^?", "o^?", "O^~",
-    "o^~", "O^.", "o^.", "O*'", "o*'", "O*`", "o*`", "O*?", "o*?", "O*~", "o*~",
-    "O*.", "o*.", "U.",  "u.",  "U?",  "u?",  "U*'", "u*'", "U*`", "u*`", "U*?",
-    "u*?", "U*~", "u*~", "U*.", "u*.", "Y`",  "y`",  "Y.",  "y.",  "Y?",  "y?",
-    "Y~",  "y~"};
+    "A(`", "A(.", "u*.", "U*.", "u*~", "U*~", "u*?", "U*?", "u*`", "U*`", "u*'",
+    "U*'", "o*.", "O*.", "o*~", "O*~", "o*?", "O*?", "o*`", "O*`", "o*'", "O*'",
+    "o^.", "O^.", "o^~", "O^~", "o^?", "O^?", "o^`", "O^`", "o^'", "O^'", "e^.",
+    "E^.", "e^~", "E^~", "e^?", "E^?", "e^`", "E^`", "e^'", "E^'", "a(.", "a(~",
+    "A(~", "a(?", "A(?", "a(`", "A^'", "a^'", "A^`", "a^`", "A^?", "a^?", "A^~",
+    "a^~", "A^.", "a^.", "A('", "a('", "A`",  "y~",  "A?",  "a.",  "A.",  "u*",
+    "A'",  "U*",  "E.",  "e.",  "E?",  "e?",  "E~",  "e~",  "o*",  "O*",  "u~",
+    "U~",  "i~",  "I~",  "dd",  "DD",  "a(",  "A(",  "I?",  "i?",  "I.",  "i.",
+    "O.",  "o.",  "O?",  "o?",  "y'",  "u'",  "u`",  "o~",  "o^",  "o'",  "o`",
+    "i'",  "i`",  "e^",  "e'",  "e`",  "a~",  "a^",  "a'",  "a`",  "Y'",  "U'",
+    "U`",  "O~",  "U.",  "u.",  "U?",  "u?",  "O^",  "O'",  "O`",  "I'",  "I`",
+    "E^",  "E'",  "E`",  "A~",  "A^",  "Y`",  "y`",  "Y.",  "y.",  "Y?",  "y?",
+    "Y~",  "a?",
+};
 
 std::vector<std::string> utf8Chars = {
-    "À", "Á", "Â", "Ã", "È", "É", "Ê", "Ì", "Í", "Ò", "Ó", "Ô", "Õ", "Ù", "Ú",
-    "Ý", "à", "á", "â", "ã", "è", "é", "ê", "ì", "í", "ò", "ó", "ô", "õ", "ù",
-    "ú", "ý", "Ă", "ă", "Ð", "đ", "Ĩ", "ĩ", "Ũ", "ũ", "Ơ", "ơ", "Ư", "ư", "Ạ",
-    "ạ", "Ả", "ả", "Ấ", "ấ", "Ầ", "ầ", "Ẩ", "ẩ", "Ẫ", "ẫ", "Ậ", "ậ", "Ắ", "ắ",
-    "Ằ", "ằ", "Ẳ", "ẳ", "Ẵ", "ẵ", "Ặ", "ặ", "Ẹ", "ẹ", "Ẻ", "ẻ", "Ẽ", "ẽ", "Ế",
-    "ế", "Ề", "ề", "Ể", "ể", "Ễ", "ễ", "Ệ", "ệ", "Ỉ", "ỉ", "Ị", "ị", "Ọ", "ọ",
-    "Ỏ", "ỏ", "Ố", "ố", "Ồ", "ồ", "Ổ", "ổ", "Ỗ", "ỗ", "Ộ", "ộ", "Ớ", "ớ", "Ờ",
-    "ờ", "Ở", "ở", "Ỡ", "ỡ", "Ợ", "ợ", "Ụ", "ụ", "Ủ", "ủ", "Ứ", "ứ", "Ừ", "ừ",
-    "Ử", "ử", "Ữ", "ữ", "Ự", "ự", "Ỳ", "ỳ", "Ỵ", "ỵ", "Ỷ", "ỷ", "Ỹ", "ỹ"};
+    "Ằ", "Ặ", "ự", "Ự", "ữ", "Ữ", "ử", "Ử", "ừ", "Ừ", "ứ", "Ứ", "ợ", "Ợ", "ỡ",
+    "Ỡ", "ở", "Ở", "ờ", "Ờ", "ớ", "Ớ", "ộ", "Ộ", "ỗ", "Ỗ", "ổ", "Ổ", "ồ", "Ồ",
+    "ố", "Ố", "ệ", "Ệ", "ễ", "Ễ", "ể", "Ể", "ề", "Ề", "ế", "Ế", "ặ", "ẵ", "Ẵ",
+    "ẳ", "Ẳ", "ằ", "Ấ", "ấ", "Ầ", "ầ", "Ẩ", "ẩ", "Ẫ", "ẫ", "Ậ", "ậ", "Ắ", "ắ",
+    "À", "ỹ", "Ả", "ạ", "Ạ", "ư", "Á", "Ư", "Ẹ", "ẹ", "Ẻ", "ẻ", "Ẽ", "ẽ", "ơ",
+    "Ơ", "ũ", "Ũ", "ĩ", "Ĩ", "đ", "Ð", "ă", "Ă", "Ỉ", "ỉ", "Ị", "ị", "Ọ", "ọ",
+    "Ỏ", "ỏ", "ý", "ú", "ù", "õ", "ô", "ó", "ò", "í", "ì", "ê", "é", "è", "ã",
+    "â", "á", "à", "Ý", "Ú", "Ù", "Õ", "Ụ", "ụ", "Ủ", "ủ", "Ô", "Ó", "Ò", "Í",
+    "Ì", "Ê", "É", "È", "Ã", "Â", "Ỳ", "ỳ", "Ỵ", "ỵ", "Ỷ", "ỷ", "Ỹ", "ả",
+};
 
 std::string viqrToUtf8(const std::string& viqr) {
-  // Temporarily use a vector of pairs to be able to sort by sequence length
-  std::vector<std::pair<std::string, std::string>> pairs;
-  for (size_t i = 0; i < viqrChars.size(); ++i) {
-    pairs.emplace_back(viqrChars[i], utf8Chars[i]);
-  }
-
-  // Sort pairs by the length of the first element in descending order
-  std::sort(pairs.begin(), pairs.end(),
-            [](const std::pair<std::string, std::string>& a,
-               const std::pair<std::string, std::string>& b) {
-              return a.first.length() > b.first.length();
-            });
-
   std::string utf8;
-  for (size_t i = 0; i < viqr.length();) {
+  for (size_t i = 0; i < viqr.length();) {  // Iterate through the VIQR string
     bool matched = false;
 
-    // Check for multi-character sequences first, now guaranteed longer
-    // sequences first
-    for (const auto& pair : pairs) {
-      if (viqr.substr(i, pair.first.length()) == pair.first) {
-        utf8 += pair.second;
-        i += pair.first.length();
-        matched = true;
-        break;  // Found the longest matching sequence at this position
+    for (size_t j = 0; j < viqrChars.size();
+         ++j) {  // Iterate through the VIQR characters
+      if (viqr.substr(i, viqrChars[j].length()) ==
+          viqrChars[j]) {            // Check if the VIQR character is found
+        utf8 += utf8Chars[j];        // Append the corresponding UTF-8 character
+        i += viqrChars[j].length();  // Move to the next character
+        matched = true;              // Set the matched flag to true
+        break;
       }
     }
 
-    // If no multi-character sequence matched, process single characters
-    if (!matched) {
-      utf8 += viqr[i];
-      i++;
+    if (!matched) {     // If the VIQR character is not found
+      utf8 += viqr[i];  // Append the character itself
+      i++;              // Move to the next character
     }
   }
 
@@ -72,13 +64,18 @@ std::string viqrToUtf8(const std::string& viqr) {
 }
 
 std::string utf8ToViqr(const std::string& utf8) {
-  std::string result = utf8;
-  for (size_t i = 0; i < utf8Chars.size(); ++i) {
-    std::string::size_type pos = 0;
-    while ((pos = result.find(utf8Chars[i], pos)) != std::string::npos) {
-      result.replace(pos, utf8Chars[i].length(), viqrChars[i]);
-      pos += viqrChars[i].length();
+  std::string result = utf8;  // The result string
+  for (size_t i = 0; i < utf8Chars.size();
+       ++i) {                        // Iterate through the UTF-8 characters
+    std::string::size_type pos = 0;  // The position of the found character
+    while ((pos = result.find(utf8Chars[i], pos)) !=
+           std::string::npos) {  // Find the UTF-8 character
+      result.replace(
+          pos, utf8Chars[i].length(),
+          viqrChars[i]);  // Replace the UTF-8 character with the VIQR character
+      pos += viqrChars[i].length();  // Move to the next character
     }
   }
+
   return result;
 }
